c48b444befb6bc5edf787f86190aad3f406fba74
George Kaye <georgejkaye@gmail.com>
2025-10-12 17:16:10 +0100

    feat: make codegen entrypoint use args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+INPUT_SCRIPT_DIR=$1
+OUTPUT_PROJECT_ROOT_DIR=$2
+OUTPUT_MODULE=$3
+
 poetry run python \
     -u \
     src/watcher/main.py \
-    /app/internal \
-    /app/user \
-    /app/output \
-    $GENERATED_CODE_MODULE
+    $INPUT_SCRIPT_DIR \
+    $OUTPUT_PROJECT_ROOT_DIR \
+    $OUTPUT_MODULE 

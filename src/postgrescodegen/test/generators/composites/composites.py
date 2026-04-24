f699ed523d28a70c3bfc0c21d6beaa31411b9430
George Kaye <georgejkaye@gmail.com>
2026-04-24 22:11:06 +0100

    test: change to using pytest

--- src/postgrescodegen/test/generators/composites/composites.py
+++ src/postgrescodegen/test/generators/composites/composites.py
@@ -1,5 +1,6 @@
+import pytest
+
 from pathlib import Path
-import unittest
 
 from postgrescodegen.generators.composites import (
     get_python_module_for_postgres_composite_file,
@@ -8,24 +9,19 @@ from postgrescodegen.generators.composites import (
 test_dir = Path(__file__).parent
 
 
-class GeneratorsCompositesTests(unittest.TestCase):
-    def one(self):
-        (_, module) = get_python_module_for_postgres_composite_file(
-            test_dir, "db", {}, test_dir / "one.sql"
-        )
+def test_one():
+    (_, module) = get_python_module_for_postgres_composite_file(
+        test_dir, "db", {}, test_dir / "one.sql"
+    )
 
-        self.assertEqual(len(module.module_objects), 1)
+    assert len(module.module_objects) == 1
 
-        type_a = module.module_objects[0]
+    type_a = module.module_objects[0]
 
-        self.assertEqual(len(type_a.composite_fields), 2)
+    assert len(type_a.composite_fields) == 2
 
-        self.assertEqual(type_a.composite_fields[0].field_name, "field_a")
-        self.assertEqual(type_a.composite_fields[0].field_type, "INTEGER")
+    assert type_a.composite_fields[0].field_name == "field_a"
+    assert type_a.composite_fields[0].field_type == "INTEGER"
 
-        self.assertEqual(type_a.composite_fields[1].field_name, "field_b")
-        self.assertEqual(type_a.composite_fields[1].field_type, "TEXT")
-
-
-if __name__ == "__main__":
-    unittest.main()
+    assert type_a.composite_fields[1].field_name == "field_b"
+    assert type_a.composite_fields[1].field_type == "TEXT"

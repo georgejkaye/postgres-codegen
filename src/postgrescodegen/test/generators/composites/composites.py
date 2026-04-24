2d545769292789dc02312708fe9af20e61563ca5
George Kaye <georgejkaye@gmail.com>
2026-04-24 22:07:42 +0100

    test: continue with test

--- src/postgrescodegen/test/generators/composites/composites.py
+++ src/postgrescodegen/test/generators/composites/composites.py
@@ -9,12 +9,22 @@ test_dir = Path(__file__).parent
 
 
 class GeneratorsCompositesTests(unittest.TestCase):
-    def test_get_postgres_composites_for_postgres_statements(self):
+    def one(self):
         (_, module) = get_python_module_for_postgres_composite_file(
-            test_dir, "db", {}, test_dir / "test.sql"
+            test_dir, "db", {}, test_dir / "one.sql"
         )
 
-        self.assertEqual(len(module.module_objects), 2)
+        self.assertEqual(len(module.module_objects), 1)
+
+        type_a = module.module_objects[0]
+
+        self.assertEqual(len(type_a.composite_fields), 2)
+
+        self.assertEqual(type_a.composite_fields[0].field_name, "field_a")
+        self.assertEqual(type_a.composite_fields[0].field_type, "INTEGER")
+
+        self.assertEqual(type_a.composite_fields[1].field_name, "field_b")
+        self.assertEqual(type_a.composite_fields[1].field_type, "TEXT")
 
 
 if __name__ == "__main__":

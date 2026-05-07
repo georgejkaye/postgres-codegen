3fef0f9881d4fab71bc21fb4921cd2dfe2a6fe68
George Kaye <georgejkaye@gmail.com>
2026-05-07 21:18:11 +0100

    feat: update main example

--- bin/main.ml
+++ bin/main.ml
@@ -1,23 +1,12 @@
 open Postgres_codegen
 open Core
-open Postgres.Function
+open Util
 
 let () =
   let function_file_contents =
     File.Concrete.Base_file_wrapper.read_file
       "../train-tracker/db/code/functions/select/train/leg.sql"
   in
-  let functions =
-    Parser.Function.Postgres_function_parser_processor
-    .get_postgres_objects_for_postgres_file_string function_file_contents
-  in
-  List.iter functions ~f:(fun f -> printf "%s\n" f.function_name);
-  let type_file_contents =
-    File.Concrete.Base_file_wrapper.read_file
-      "../train-tracker/db/code/types/train/leg.sql"
-  in
-  let types =
-    Parser.Composite.Postgres_composite_parser_processor
-    .get_postgres_objects_for_postgres_file_string type_file_contents
-  in
-  List.iter types ~f:(fun f -> printf "%s\n" f.composite_name)
+  function_file_contents
+  |> Parser.Process.get_postgres_objects_for_file_contents
+  |> List.iter ~f:(Show.show_line Postgres.Object.show_postgres_object)

d1e1014ee2eb34d5b2302a51ae9622bfbd01c917
George Kaye <georgejkaye@gmail.com>
2026-06-09 22:04:58 +0100

    feat: set up main method to use new function

--- bin/main.ml
+++ bin/main.ml
@@ -1,31 +1,33 @@
 open Postgres_codegen
 open Core
-open Util
 
-let param =
+let params =
   let open Command.Param in
-  anon ("filename" %: string)
+  both (anon ("filename" %: string)) (anon ("base" %: string))
 
-let read_file = File.Wrapper.Base_file_wrapper.read_file
-let get_postgres_objects = Parser.Process.parse_file_contents
-let print_statement = Show.show_line Postgres.Statement.show_statement
-let print_message = Show.show_line (fun x -> x)
+module Parser = Parser.Process.Process (File.Wrapper.Base_file_wrapper)
+
+let get_postgres_objects = Parser.postgres_module_of_file
+let print_message = Util.Show.show_line (fun x -> x)
 
 let print_error_or_result = function
   | Second msg -> print_message msg
-  | First objects -> List.iter ~f:print_statement objects
-
-let get_objects = function
-  | Second msg -> Second msg
-  | First contents -> (
-      match get_postgres_objects contents with
-      | Ok vs -> First vs
-      | Error msg -> Second msg)
+  | First objects ->
+      let open Postgres.Module in
+      List.iter
+        ~f:(Util.Show.show_line Postgres.Statement.show_statement)
+        objects.statements
 
 let command =
   Command.basic ~summary:"Generate code for interacting with postgres objects"
     ~readme:(fun () -> "Todo")
-    (Command.Param.map param ~f:(fun filename () ->
-         filename |> read_file |> get_objects |> print_error_or_result))
+    (Command.Param.map params ~f:(fun (file_path, base_path) () ->
+         let file_path = Util.File.of_string file_path in
+         let base_path = Util.File.of_string base_path in
+         match (file_path, base_path) with
+         | First file_path, First base_path ->
+             get_postgres_objects ~file_path ~base_path |> print_error_or_result
+         | Second _, _ -> print_message "Could not parse file path"
+         | _, Second _ -> failwith "Could not parse base path"))
 
 let () = Command_unix.run ~version:"1.0" ~build_info:"RWO" command

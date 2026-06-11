8783a30580b8fed1e1d2c98cc0b0ba912401e82a
George Kaye <georgejkaye@gmail.com>
2026-06-11 21:45:26 +0100

    test: add more test data

--- test/data/function.sql
+++ test/data/function.sql
@@ -1,11 +1,17 @@
-CREATE FUNCTION test_function (
-    p_arg_one INTEGER,
-    p_arg_two TEXT,
-    p_arg_three TIMESTAMP WITH TIME ZONE
-)
-RETURNS INTEGER
+CREATE FUNCTION test_function (p_one TEXT)
+RETURNS SETOF details_detal
+LANGUAGE sql
+AS
+SELECT 1 FROM DUAL;
+--test
+CREATE FUNCTION test_function2 (p_two TEXT_NOTNULL, p_three INTEGER[])
+RETURNS SETOF details_detal
 LANGUAGE sql
 AS
 $$
-SELECT * FROM test_table;
+DECLARE
+    v_four TEXT ----test
+BEGIN
+    INSERT INTO test (c_one) VALUES (v_four);
+END;
 $$;

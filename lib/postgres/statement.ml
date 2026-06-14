c64b0fdd57e3a954457c995bc9dc836de0d0e07f
George Kaye <georgejkaye@gmail.com>
2026-06-14 17:48:28 +0100

    feat: add or replace to create statement

--- lib/postgres/statement.ml
+++ lib/postgres/statement.ml
@@ -1,9 +1,16 @@
+type create_parameters = {
+  object_data : Object.postgres_object;
+  or_replace : bool;
+}
+[@@deriving show]
+
 type drop_parameters = {
   object_type : Object_type.postgres_object_type;
   object_name : string;
+  if_exists : bool;
   cascade : bool;
 }
 [@@deriving show]
 
-type statement = Create of Object.postgres_object | Drop of drop_parameters
+type statement = Create of create_parameters | Drop of drop_parameters
 [@@deriving show]
